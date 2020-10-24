1cd72d329988ecd8562d88e74720c523e1ca2c9b
Eduardo Valencia <8025918@gmail.com>
2020-10-23 19:17:24 -0700

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -10,6 +10,8 @@ import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler, NotFoundError } from '@tickets-448800/common'
 
+console.log('starting auth service')
+
 const app = express()
 app.set('trust proxy', true)
 
